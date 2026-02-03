2fd114401c9786e3594306eeb6cf8f422347a737
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-03 15:35:28 -0300

    :ok_hand:	style: aplica padronização de formatação no código
    Ajusta a indentação e o estilo de formatação em múltiplos arquivos
    para seguir as regras definidas pelo Prettier, garantindo consistência visual e reduzindo diferenças desnecessárias nos diffs.

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
-  },
 })
