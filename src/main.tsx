6bf4a3d44d327d49b69ed0556378dd800f1f5733
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-04 17:41:48 -0300

    ♻️ refactor: utiliza componente Providers no bootstrap da aplicação
    Substitui o uso direto do `Provider` do Redux pelo componente `Providers`, centralizando a configuração de contextos globais e deixando o ponto de entrada da aplicação mais limpo e escalável.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from '@/app/store'
+import { Providers } from '@/app/providers'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider store={store}></Provider>
+        <Providers></Providers>
     </StrictMode>,
 )
