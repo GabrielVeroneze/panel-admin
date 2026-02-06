2da8f9f1e32ac2193973c560d3af4d6fa41ce5e6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-06 16:59:44 -0300

    ✨ feat: adiciona componente AuthenticatedHeader
    Cria o componente AuthenticatedHeader utilizando componentes compartilhados como Input, IconButton e UserAvatar, organizando a área de ações do usuário com notificações e avatar no layout para usuários autenticados.

---
+++ src/shared/layout/Header/AuthenticatedHeader/AuthenticatedHeader.tsx
@@ -0,0 +1,15 @@
+import { IconButton, Input, UserAvatar } from '@/shared/components'
+import { BellSolidIcon } from '@/shared/icons'
+import styles from './AuthenticatedHeader.module.scss'
+
+export const AuthenticatedHeader = () => {
+    return (
+        <div className={styles.container}>
+            <Input />
+            <div className={styles.actions}>
+                <IconButton icon={<BellSolidIcon />} ariaLabel="Notificações" />
+                <UserAvatar src="" />
+            </div>
+        </div>
+    )
+}
