e847df4ac1fcb1fc67a639881358254f3678e411
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-08 18:36:05 -0300

    ✨ feat: permite className customizada no componente Input
    Atualiza o componente Input para aceitar e mesclar uma className
    externa com os estilos internos, aumentando a flexibilidade de
    estilização em diferentes contextos da aplicação.

--- src/shared/components/Input/Input.tsx
+++ src/shared/components/Input/Input.tsx
@@ -3,6 +3,6 @@ import styles from './Input.module.scss'
 
 type InputProps = ComponentProps<'input'>
 
-export const Input = (props: InputProps) => {
-    return <input className={styles.input} {...props} />
+export const Input = ({ className, ...props }: InputProps) => {
+    return <input className={`${styles.input} ${className}`} {...props} />
 }
