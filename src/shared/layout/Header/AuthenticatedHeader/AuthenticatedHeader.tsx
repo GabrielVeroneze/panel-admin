a9ae7821c66b353909e89a3a64eea58c82b19af1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-12 15:28:13 -0300

    ✨ feat: adiciona dropdown de usuário no header autenticado
    Atualiza o AuthenticatedHeader para incluir um Dropdown
    associado ao UserAvatar, adicionando ações como perfil,
    configurações, logout e exclusão, além de exibição de
    informações do usuário.

--- src/shared/layout/Header/AuthenticatedHeader/AuthenticatedHeader.tsx
+++ src/shared/layout/Header/AuthenticatedHeader/AuthenticatedHeader.tsx
@@ -1,5 +1,18 @@
-import { IconButton, Input, UserAvatar } from '@/shared/components'
-import { BellSolidIcon } from '@/shared/icons'
+import {
+    Dropdown,
+    DropdownDivider,
+    DropdownItem,
+    IconButton,
+    Input,
+    UserAvatar,
+} from '@/shared/components'
+import {
+    BellSolidIcon,
+    CogSolidIcon,
+    LogoutIcon,
+    TrashSolidIcon,
+    UserSolidIcon,
+} from '@/shared/icons'
 import styles from './AuthenticatedHeader.module.scss'
 
 export const AuthenticatedHeader = () => {
@@ -8,7 +21,30 @@ export const AuthenticatedHeader = () => {
             <Input className={styles.input} />
             <div className={styles.actions}>
                 <IconButton icon={<BellSolidIcon />} ariaLabel="Notificações" />
-                <UserAvatar src="" />
+                <Dropdown
+                    trigger={
+                        <UserAvatar src="https://i.pravatar.cc/300?u=a04258213sdfg40fw2" />
+                    }
+                >
+                    <DropdownItem disabled>
+                        <div className={styles.userInfo}>
+                            <strong>Gabriel Veroneze</strong>
+                            <span>gabriel@email.com</span>
+                        </div>
+                    </DropdownItem>
+                    <DropdownDivider />
+                    <DropdownItem icon={<UserSolidIcon />}>
+                        Your Profile
+                    </DropdownItem>
+                    <DropdownItem icon={<CogSolidIcon />}>
+                        Settings
+                    </DropdownItem>
+                    <DropdownItem icon={<LogoutIcon />}>Sign out</DropdownItem>
+                    <DropdownDivider />
+                    <DropdownItem icon={<TrashSolidIcon />} variant="danger">
+                        Delete
+                    </DropdownItem>
+                </Dropdown>
             </div>
         </div>
     )
