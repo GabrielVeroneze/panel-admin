a80db559380067cefa43d4ca5edb0868b85d3111
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-16 15:34:10 -0300

    ✨ feat: cria componente Card polimórfico
    Implementa o componente Card com suporte à prop "as",
    permitindo renderização polimórfica e herança de props
    nativas do elemento definido. Estabelece uma base flexível
    para composição de layouts e containers reutilizáveis.

---
+++ src/shared/components/Card/Card.tsx
@@ -0,0 +1,21 @@
+import type { ComponentPropsWithoutRef, ElementType } from 'react'
+import styles from './Card.module.scss'
+
+type CardProps<T extends ElementType = 'div'> = {
+    as?: T
+} & ComponentPropsWithoutRef<T>
+
+export const Card = <T extends ElementType = 'div'>({
+    children,
+    as,
+    className,
+    ...props
+}: CardProps<T>) => {
+    const Component = as || 'div'
+
+    return (
+        <Component className={`${styles.card} ${className}`} {...props}>
+            {children}
+        </Component>
+    )
+}
