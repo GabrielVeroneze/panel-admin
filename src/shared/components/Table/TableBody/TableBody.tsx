c188de2cb3407d955fb27670da03e222d9936e4e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-17 18:39:06 -0300

    ♻️ refactor: remove estilos do TableBody
    Simplifica o componente TableBody removendo a importação de estilos e a classe CSS associada, mantendo apenas
    a renderização direta do elemento <tbody> com repasse de props nativas.

--- src/shared/components/Table/TableBody/TableBody.tsx
+++ src/shared/components/Table/TableBody/TableBody.tsx
@@ -1,12 +1,7 @@
 import type { ComponentProps } from 'react'
-import styles from './TableBody.module.scss'
 
 type TableBodyProps = ComponentProps<'tbody'>
 
 export const TableBody = ({ children, ...props }: TableBodyProps) => {
-    return (
-        <tbody className={styles.tableBody} {...props}>
-            {children}
-        </tbody>
-    )
+    return <tbody {...props}>{children}</tbody>
 }
