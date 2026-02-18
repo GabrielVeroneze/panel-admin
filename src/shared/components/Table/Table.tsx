d05914d13977037c75c173227f691e35a38598fa
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-18 15:13:42 -0300

    ✨ feat: adiciona suporte a definição dinâmica de colunas
    Atualiza o componente Table para aceitar a nova prop "columns",
    permitindo configurar dinamicamente a quantidade/tamanho das
    colunas via CSS custom property (--table-columns).

--- src/shared/components/Table/Table.tsx
+++ src/shared/components/Table/Table.tsx
@@ -1,17 +1,21 @@
-import type { ComponentProps } from 'react'
+import type { ComponentProps, CSSProperties } from 'react'
 import styles from './Table.module.scss'
 
 type TableProps = {
+    columns?: string
     striped?: boolean
 } & ComponentProps<'table'>
 
-export const Table = ({ children, striped, ...props }: TableProps) => {
+export const Table = ({ children, columns, striped, ...props }: TableProps) => {
+    const style = { '--table-columns': columns } as CSSProperties
+
     return (
         <table
             className={`
                 ${styles.table}
                 ${striped ? styles.striped : ''}
             `}
+            style={style}
             {...props}
         >
             {children}
