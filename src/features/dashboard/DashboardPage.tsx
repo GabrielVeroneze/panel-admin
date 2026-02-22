a829ec5b01af44e4f15963767e48278a75fcd0a8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-02-22 13:44:16 -0300

    ✨ feat: implementa estrutura inicial do Dashboard
    Substitui o placeholder da DashboardPage por uma estrutura
    composta por múltiplos cards e widgets, incluindo gráficos,
    listas e tabelas, organizados por meio de estilos específicos
    da página.

--- src/features/dashboard/DashboardPage.tsx
+++ src/features/dashboard/DashboardPage.tsx
@@ -1,3 +1,43 @@
+import { Card } from '@/shared/components'
+import {
+    LatestCustomersList,
+    SalesChart,
+    SessionsByCountry,
+    SessionsByDevice,
+    WeekVisitors,
+    TodaySales,
+    TodayVisitors,
+    TransactionsTable,
+} from './components'
+import styles from './DashboardPage.module.scss'
+
 export const DashboardPage = () => {
-    return <div>DashboardPage</div>
+    return (
+        <section className={styles.dashboard}>
+            <Card className={styles.sales}>
+                <SalesChart />
+            </Card>
+            <Card className={styles.todaySales}>
+                <TodaySales />
+            </Card>
+            <Card className={styles.todayVisitors}>
+                <TodayVisitors />
+            </Card>
+            <Card className={styles.weekVisitors}>
+                <WeekVisitors />
+            </Card>
+            <Card className={styles.sessionsByCountry}>
+                <SessionsByCountry />
+            </Card>
+            <Card className={styles.latestCustomers}>
+                <LatestCustomersList />
+            </Card>
+            <Card className={styles.sessionsByDevice}>
+                <SessionsByDevice />
+            </Card>
+            <Card className={styles.transactions}>
+                <TransactionsTable />
+            </Card>
+        </section>
+    )
 }
