2ca2e533df0c11046350cfd3a60a11980f1a5a94
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-03-06 15:26:26 -0300

    :wastebasket: remove: deleta função de estilo baseada em GeoJSON do Leaflet
    Remove a função `geoJsonStyle`, utilizada para definir estilos de camadas GeoJSON no Leaflet, após a migração da implementação do mapa.

--- src/features/dashboard/components/SessionsByCountry/map/map.style.ts
+++ src/features/dashboard/components/SessionsByCountry/map/map.style.ts
@@ -1,24 +1,6 @@
 import { maxSessions, sessionsMap } from '../sessions.data'
-import { getColor } from '../sessions.utils'
-import type { Feature } from 'geojson'
 
-export const geoJsonStyle  = (feature?: Feature) => {
-    if (!feature) {
-        return {
-            fillColor: '#d1d5db',
-            weight: 1,
-            color: '#ffffff',
-            fillOpacity: 1,
-        }
-    }
 
-    const isoCode = feature.properties?.iso_a2
     const sessions = sessionsMap.get(isoCode) ?? 0
 
-    return {
-        fillColor: getColor(sessions, maxSessions),
-        weight: 1,
-        color: '#ffffff',
-        fillOpacity: 1,
-    }
 }
