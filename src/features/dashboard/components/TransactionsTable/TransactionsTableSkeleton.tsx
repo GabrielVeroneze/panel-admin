c20cf7b5f6a21d1fe2051c41757d0700c0d954f3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-03-16 22:38:05 -0300

    ♻️ refactor: move dimensões e layout para CSS
    Atualiza o `TransactionsTableSkeleton` para utilizar classes CSS em vez de definir `width`, `height` e estilos inline diretamente no JSX. Também aplica a variação `styles.skeleton` no container com `clsx`, melhorando organização, consistência e reutilização dos estilos.

--- src/features/dashboard/components/TransactionsTable/TransactionsTableSkeleton.tsx
+++ src/features/dashboard/components/TransactionsTable/TransactionsTableSkeleton.tsx
@@ -1,27 +1,26 @@
 import { Skeleton } from '@/shared/components'
+import clsx from 'clsx'
 import styles from './TransactionsTable.module.scss'
 
 const ROWS = 7
 
 export const TransactionsTableSkeleton = () => {
     return (
-        <div className={styles.container}>
+        <div className={clsx(styles.container, styles.skeleton)}>
             <header className={styles.header}>
                 <div className={styles.title}>
-                    <Skeleton width={110} height={20} />
+                    <Skeleton className={styles.titleSkeleton} />
                 </div>
-                <Skeleton width={200} height={15} />
+                <Skeleton className={styles.textSkeleton} />
             </header>
             <div className={styles.tableContainer}>
-                <div
-                    style={{ display: 'flex', flexDirection: 'column', gap: 2 }}
-                >
+                <div className={styles.tableSkeleton}>
                     {Array.from({ length: ROWS }).map((_, index) => (
                         <div key={index} style={{ display: 'flex', gap: 2 }}>
-                            <Skeleton width="42%" height={42} />
-                            <Skeleton width="28%" height={42} />
-                            <Skeleton width="15%" height={42} />
-                            <Skeleton width="15%" height={42} />
+                            <Skeleton className={styles.transactionSkeleton} />
+                            <Skeleton className={styles.dateSkeleton} />
+                            <Skeleton className={styles.amountSkeleton} />
+                            <Skeleton className={styles.statusSkeleton} />
                         </div>
                     ))}
                 </div>
