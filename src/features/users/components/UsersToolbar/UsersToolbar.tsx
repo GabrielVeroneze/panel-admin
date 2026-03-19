e8aafefb3f60c4ef1d96f5f31a3843bb0a29ec1a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-03-19 17:30:42 -0300

    ✨ feat: implementa toolbar de usuários com ações e busca
    Estrutura o componente `UsersToolbar` com campo de busca, ações com `IconButton` e botão de exportação, melhorando a usabilidade e organização da interface.

--- src/features/users/components/UsersToolbar/UsersToolbar.tsx
+++ src/features/users/components/UsersToolbar/UsersToolbar.tsx
@@ -1,5 +1,36 @@
+import { Button, IconButton, Input } from '@/shared/components'
+import {
+    CogIcon,
+    DocumentDownloadSolidIcon,
+    DotsVerticalSolidIcon,
+    ExclamationCircleSolidIcon,
+    TrashSolidIcon,
+} from '@/shared/assets/icons'
 import styles from './UsersToolbar.module.scss'
 
 export const UsersToolbar = () => {
-    return <div>UsersToolbar</div>
+    return (
+        <div className={styles.container}>
+            <Input
+                className={styles.input}
+                size="large"
+                placeholder="Search for users"
+            />
+            <div className={styles.actions}>
+                <IconButton icon={<CogIcon />} />
+                <IconButton icon={<TrashSolidIcon />} />
+                <IconButton icon={<ExclamationCircleSolidIcon />} />
+                <IconButton icon={<DotsVerticalSolidIcon />} />
+            </div>
+            <Button
+                className={styles.button}
+                size="lg"
+                variant="transparent"
+                iconPosition="left"
+                icon={<DocumentDownloadSolidIcon />}
+            >
+                Export
+            </Button>
+        </div>
+    )
 }
