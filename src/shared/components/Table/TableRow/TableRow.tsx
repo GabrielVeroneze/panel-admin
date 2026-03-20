1ed1ad3bfd74f0e628bbe10e5dd8655d24dccb9d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-03-19 22:47:52 -0300

    ✨ feat: adiciona suporte a variações de tamanho no TableRow
    Inclui a prop `size` no componente `TableRow`, permitindo ajustar o espaçamento das linhas através de variantes (`sm`, `md`, `lg`).

--- src/shared/components/Table/TableRow/TableRow.tsx
+++ src/shared/components/Table/TableRow/TableRow.tsx
@@ -1,11 +1,18 @@
 import type { ComponentProps } from 'react'
 import styles from './TableRow.module.scss'
+import clsx from 'clsx'
 
-type TableRowProps = ComponentProps<'tr'>
+type TableRowProps = {
+    size?: 'sm' | 'md' | 'lg'
+} & ComponentProps<'tr'>
 
-export const TableRow = ({ children, ...props }: TableRowProps) => {
+export const TableRow = ({
+    children,
+    size = 'md',
+    ...props
+}: TableRowProps) => {
     return (
-        <tr className={styles.tableRow} {...props}>
+        <tr className={clsx(styles.tableRow, styles[size])} {...props}>
             {children}
         </tr>
     )
