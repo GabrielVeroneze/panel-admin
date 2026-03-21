e6851c2b886aa7cbf0b8dd80e033e217e747fc0f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-03-20 21:54:18 -0300

    ✨ feat: implementa rodapé de usuários com paginação e informações
    Estrutura o componente `UsersFooter` com `TablePagination` e seção informativa de atividade recente, incluindo ícone e estilos associados.

--- src/features/users/components/UsersFooter/UsersFooter.tsx
+++ src/features/users/components/UsersFooter/UsersFooter.tsx
@@ -1,5 +1,24 @@
+import { TablePagination } from '@/shared/components'
+import { EyeSolidIcon } from '@/shared/assets/icons'
 import styles from './UsersFooter.module.scss'
 
 export const UsersFooter = () => {
-    return <div>UsersFooter</div>
+    return (
+        <div className={styles.footer}>
+            <TablePagination
+                variant="withTotal"
+                page={1}
+                pageSize={15}
+                total={987}
+                onPageChange={() => {}}
+            />
+            <div className={styles.info}>
+                <span className={styles.text}>
+                    Last account activity:{' '}
+                    <strong className={styles.time}>2 hours ago</strong>
+                </span>
+                <EyeSolidIcon className={styles.icon} />
+            </div>
+        </div>
+    )
 }
