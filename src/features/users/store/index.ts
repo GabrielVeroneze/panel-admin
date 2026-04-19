1dd35f43625930f22d0e84856759eaf2d330ad08
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-04-19 17:17:48 -0300

    ✨ feat: expõe ação deleteUsers no módulo de usuários
    Adiciona deleteUsers aos exports do slice, permitindo utilizar a exclusão em lote de usuários.

--- src/features/users/store/index.ts
+++ src/features/users/store/index.ts
@@ -1,3 +1,9 @@
 export { default as usersReducer } from './users.slice'
-export { createUser, deleteUser, fetchUsers, updateUser } from './users.slice'
+export {
+    createUser,
+    deleteUser,
+    deleteUsers,
+    fetchUsers,
+    updateUser,
+} from './users.slice'
 export { selectUsersList } from './users.selectors'
