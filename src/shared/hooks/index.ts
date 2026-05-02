6a9004ca443a3db33c6fc0d2b3cd80a15263d8b7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-05-02 18:35:24 -0300

    ✨ feat: exporta hook useFormModal
    Adiciona exportação do hook genérico de controle de modais, permitindo reutilização em diferentes formulários.

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useBreakpoint } from './useBreakpoint'
 export { useDataSelection } from './useDataSelection'
+export { useFormModal } from './useFormModal'
 export { usePaginationFilters } from './usePaginationFilters'
