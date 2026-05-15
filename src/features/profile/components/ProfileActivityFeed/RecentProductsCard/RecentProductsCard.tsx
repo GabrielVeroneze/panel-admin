b7b20154780aa69235efb613e9a17ca496b495db
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-05-15 18:37:59 -0300

    ✨ feat: cria componente RecentProductsCard
    Adiciona card de produtos recentes utilizando ProfileSectionCard e lista de itens para exibição de produtos gerenciados recentemente.

---
+++ src/features/profile/components/ProfileActivityFeed/RecentProductsCard/RecentProductsCard.tsx
@@ -0,0 +1,31 @@
+import { ShoppingBagIcon } from '@/shared/assets/icons'
+import { ProfileSectionCard } from '@/features/profile/components'
+import { RecentProductItem } from './RecentProductItem/RecentProductItem'
+import styles from './RecentProductsCard.module.scss'
+
+type Product = {
+    id: number
+    image: string
+    name: string
+    category: string
+    price: string
+}
+
+type RecentProductsCardProps = {
+    products: Product[]
+}
+
+export const RecentProductsCard = ({ products }: RecentProductsCardProps) => {
+    return (
+        <ProfileSectionCard
+            icon={<ShoppingBagIcon />}
+            title="Recently Managed Products"
+        >
+            <div className={styles.products}>
+                {products.map((product) => (
+                    <RecentProductItem key={product.id} {...product} />
+                ))}
+            </div>
+        </ProfileSectionCard>
+    )
+}
