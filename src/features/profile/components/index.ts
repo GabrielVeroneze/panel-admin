6eafa1914251de0837e6cb9f8b7f32469335a702
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-05-17 20:32:20 -0300

    ✨ feat: exporta componente ProfileTimeline
    Adiciona ProfileTimeline às exportações centralizadas do módulo de profile para facilitar reutilização e importações.

--- src/features/profile/components/index.ts
+++ src/features/profile/components/index.ts
@@ -1,3 +1,4 @@
 export * from './ProfileActivityFeed/ProfileActivityFeed'
 export * from './ProfileSectionCard/ProfileSectionCard'
 export * from './ProfileSidebar/ProfileSidebar'
+export * from './ProfileTimeline/ProfileTimeline'
