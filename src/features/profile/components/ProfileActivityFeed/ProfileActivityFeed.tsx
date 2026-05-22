785ff9863a6bd73ac6853b13490a0360eb0f351c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-05-21 21:39:37 -0300

    ♻️ refactor: utiliza ProfileActivityFeedData no ProfileActivityFeed
    Substitui props individuais por objeto tipado de feed, simplificando interface do componente e centralizando estrutura de dados.

--- src/features/profile/components/ProfileActivityFeed/ProfileActivityFeed.tsx
+++ src/features/profile/components/ProfileActivityFeed/ProfileActivityFeed.tsx
@@ -1,38 +1,23 @@
 import { AdminSummaryCard } from './AdminSummaryCard/AdminSummaryCard'
 import { RecentActivityCard } from './RecentActivityCard/RecentActivityCard'
 import { RecentProductsCard } from './RecentProductsCard/RecentProductsCard'
-import type {
-    Activity,
-    ProfileSummary,
-    RecentProduct,
-    StatValue,
-} from '@/features/profile/types'
+import type { ProfileActivityFeedData } from '@/features/profile/types'
 import styles from './ProfileActivityFeed.module.scss'
 
 type ProfileActivityFeedProps = {
-    products: StatValue
-    users: StatValue
-    profile: ProfileSummary
-    activities: Activity[]
-    recentProducts: RecentProduct[]
+    feed: ProfileActivityFeedData
 }
 
-export const ProfileActivityFeed = ({
-    products,
-    users,
-    profile,
-    activities,
-    recentProducts,
-}: ProfileActivityFeedProps) => {
+export const ProfileActivityFeed = ({ feed }: ProfileActivityFeedProps) => {
     return (
         <section className={styles.feed}>
             <AdminSummaryCard
-                products={products}
-                users={users}
-                profile={profile}
+                products={feed.products}
+                users={feed.users}
+                profile={feed.profile}
             />
-            <RecentActivityCard activities={activities} />
-            <RecentProductsCard products={recentProducts} />
+            <RecentActivityCard activities={feed.activities} />
+            <RecentProductsCard products={feed.recentProducts} />
         </section>
     )
 }
