338a593bd4faeff092b6a14a024b50b4404735ac
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-06-06 18:35:22 -0300

    ✨ feat: amplia mapeamento visual de atividades
    Adiciona suporte aos novos tipos de atividade no sistema de configuração, associando títulos, ícones e variações visuais para representar ações relacionadas a usuários, produtos, autenticação e perfil.

--- src/features/profile/components/ProfileActivityFeed/RecentActivityCard/ActivityItem/ActivityItem.config.ts
+++ src/features/profile/components/ProfileActivityFeed/RecentActivityCard/ActivityItem/ActivityItem.config.ts
@@ -1,9 +1,12 @@
 import {
     CubeIcon,
     LockClosedIcon,
+    LoginIcon,
     PencilIcon,
     TrashIcon,
+    UploadIcon,
     UserAddIcon,
+    UserCircleIcon,
 } from '@/shared/assets/icons'
 import type { ComponentType, SVGProps } from 'react'
 import type { ActivityType } from '@/features/profile/types'
@@ -17,29 +20,54 @@ type ActivityConfigItem = {
 }
 
 export const activityConfig: Record<ActivityType, ActivityConfigItem> = {
+   'user-created': {
+        title: 'Created User',
+        icon: UserAddIcon,
+        variant: 'green',
+    },
+    'user-updated': {
+        title: 'Updated User',
+        icon: PencilIcon,
+        variant: 'blue',
+    },
+    'user-deleted': {
+        title: 'Deleted User',
+        icon: TrashIcon,
+        variant: 'red',
+    },
+    'product-created': {
+        title: 'Created Product',
+        icon: CubeIcon,
+        variant: 'green',
+    },
     'product-updated': {
         title: 'Updated Product',
         icon: PencilIcon,
         variant: 'blue',
-    },
-    'user-created': {
-        title: 'Created new user',
-        icon: UserAddIcon,
-        variant: 'green',
     },
     'product-deleted': {
         title: 'Deleted Product',
         icon: TrashIcon,
         variant: 'red',
     },
-    'stock-updated': {
-        title: 'Updated stock',
-        icon: CubeIcon,
+    'product-image-uploaded': {
+        title: 'Uploaded Product Image',
+        icon: UploadIcon,
+        variant: 'orange',
+    },
+    'profile-avatar-updated': {
+        title: 'Updated Profile Avatar',
+        icon: UserCircleIcon,
         variant: 'orange',
     },
     'password-changed': {
         title: 'Changed Password',
         icon: LockClosedIcon,
+        variant: 'purple',
+    },
+    'admin-login': {
+        title: 'Admin Login',
+        icon: LoginIcon,
         variant: 'purple',
     },
 }
