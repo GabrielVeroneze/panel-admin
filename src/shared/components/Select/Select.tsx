9e258885c91b7e223b0099e0c0981608eaa98eac
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-06-08 17:13:17 -0300

    ✨ feat: adiciona suporte a tamanhos e estados no Select
    Atualiza o componente para utilizar tipagens compartilhadas, permitindo personalização de tamanho e aplicação de estilos através de propriedades de controle.

--- src/shared/components/Select/Select.tsx
+++ src/shared/components/Select/Select.tsx
@@ -1,13 +1,25 @@
-import type { ComponentProps, ReactNode } from 'react'
+import type { ReactNode } from 'react'
+import type { FieldControlProps, FieldSize } from '@/shared/types'
+import type { NativeSelectProps } from './Select.types'
+import clsx from 'clsx'
 import styles from './Select.module.scss'
 
-type SelectProps = {
+type SelectProps = NativeSelectProps & {
     children: ReactNode
-} & ComponentProps<'select'>
+    size?: FieldSize
+} & FieldControlProps
 
-export const Select = ({ children, ...props }: SelectProps) => {
+export const Select = ({
+    children,
+    size = 'medium',
+    className,
+    ...props
+}: SelectProps) => {
     return (
-        <select className={styles.select} {...props}>
+        <select
+            className={clsx(styles.select, styles[size], className)}
+            {...props}
+        >
             {children}
         </select>
     )
