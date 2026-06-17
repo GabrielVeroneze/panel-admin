dba1c702aeb0c60e95e59066c6868143a39bf4a5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-06-17 16:58:39 -0300

    ♻️ refactor: torna SettingsToggleList baseado em composição
    Substitui a renderização baseada em lista de dados pelo uso de children, aumentando a flexibilidade e permitindo diferentes composições de itens.

--- src/features/settings/components/SettingsToggleList/SettingsToggleList.tsx
+++ src/features/settings/components/SettingsToggleList/SettingsToggleList.tsx
@@ -1,17 +1,10 @@
-import { SettingsToggleItem } from './SettingsToggleItem/SettingsToggleItem'
-import type { SettingsToggle } from '@/features/settings/types'
+import type { ReactNode } from 'react'
 import styles from './SettingsToggleList.module.scss'
 
 type SettingsToggleListProps = {
-    items: SettingsToggle[]
+    children: ReactNode
 }
 
-export const SettingsToggleList = ({ items }: SettingsToggleListProps) => {
-    return (
-        <div className={styles.list}>
-            {items.map((item) => (
-                <SettingsToggleItem key={item.id} setting={item} />
-            ))}
-        </div>
-    )
+export const SettingsToggleList = ({ children }: SettingsToggleListProps) => {
+    return <div className={styles.list}>{children}</div>
 }
