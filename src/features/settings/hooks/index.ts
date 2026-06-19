e6376e668fcd84377ee87c0a20975ebfff015bd3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-06-19 19:04:03 -0300

    ✨ feat: exporta hook de formulário de senha
    Adiciona a exportação centralizada do hook responsável pelo gerenciamento e validação do formulário de alteração de senha.

--- src/features/settings/hooks/index.ts
+++ src/features/settings/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useGeneralInformationForm } from './useGeneralInformationForm'
 export { useLanguageTimeForm } from './useLanguageTimeForm'
+export { usePasswordForm } from './usePasswordForm'
 export { useSettings } from './useSettings'
