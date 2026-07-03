72d04bc8410cea6bb99e8a4bf2b17847769feebf
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-07-02 21:57:38 -0300

    ✨ feat: adiciona hook para formulário de login
    Cria um hook para encapsular a lógica do formulário de login, integrando a validação com React Hook Form e Zod, o fluxo de autenticação e o redirecionamento após a autenticação bem-sucedida.

---
+++ src/features/auth/hooks/useSignInForm.ts
@@ -0,0 +1,34 @@
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useAuth } from './useAuth'
+import { signInSchema, type SignInFormValues } from '../schemas'
+
+const defaultValues: SignInFormValues = {
+    email: '',
+    password: '',
+    rememberMe: false,
+}
+
+export const useSignInForm = () => {
+    const navigate = useNavigate()
+
+    const { signIn } = useAuth()
+
+    const form = useForm<SignInFormValues>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: defaultValues,
+        mode: 'onTouched',
+    })
+
+    const onSubmit = async (data: SignInFormValues) => {
+        await signIn(data)
+
+        navigate('/')
+    }
+
+    return {
+        ...form,
+        onSubmit,
+    }
+}
