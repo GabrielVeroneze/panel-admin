ad639334750ec229abb5d76dec1e1b8b6fff46c8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-07-03 20:33:26 -0300

    ✨ feat: inicializa autenticação no carregamento da aplicação
    Atualiza a inicialização da aplicação para utilizar o componente responsável por restaurar a sessão do usuário autenticado durante o carregamento, além de centralizar as importações dos componentes da aplicação.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Providers } from '@/app/providers'
 import { RouterProvider } from 'react-router'
+import { AppInitializer, Providers } from '@/app'
 import { router } from '@/routes'
 import '@/styles/global.scss'
 
@@ -19,7 +19,9 @@ enableMocking().then(() => {
     createRoot(document.getElementById('root')!).render(
         <StrictMode>
             <Providers>
-                <RouterProvider router={router} />
+                <AppInitializer>
+                    <RouterProvider router={router} />
+                </AppInitializer>
             </Providers>
         </StrictMode>,
     )
