c0fa92db3a6b139eeb4fbbf030a8d4c69dbade2e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-08-10 19:19:06 -0300

    ✨ feat: cria helper para renderização de componentes
    Cria o `renderWithProviders` para padronizar a renderização de componentes durante os testes. O helper envolve os componentes com o `Provider` do Redux e o `MemoryRouter`, evitando a necessidade de repetir essa infraestrutura em cada teste. Também reexporta as funcionalidades do Testing Library para centralizar os imports relacionados aos testes.

---
+++ src/tests/test-utils.tsx
@@ -0,0 +1,19 @@
+import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router'
+import { render, type RenderOptions } from '@testing-library/react'
+import { store } from '@/store'
+import type { ReactElement } from 'react'
+
+export const renderWithProviders = (
+    ui: ReactElement,
+    options?: RenderOptions,
+) => {
+    return render(
+        <Provider store={store}>
+            <MemoryRouter>{ui}</MemoryRouter>
+        </Provider>,
+        options,
+    )
+}
+
+export * from '@testing-library/react'
