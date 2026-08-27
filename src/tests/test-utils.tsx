fa9e7db251696e135bb7bdcf1443bb33c943a52b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-08-27 19:36:36 -0300

    ♻️ refactor: usa store isolada nos testes
    Atualiza o renderizador de testes para utilizar uma store específica de teste, permitindo fornecer uma instância personalizada e evitando o compartilhamento da store da aplicação entre os testes.

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -1,18 +1,24 @@
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router'
 import { render, type RenderOptions } from '@testing-library/react'
-import { store } from '@/store'
+import { setupStore, type TestStore } from './setupStore'
 import type { ReactElement } from 'react'
 
 type RouterOptions = {
     initialEntries?: string[]
 }
 
-type CustomRenderOptions = RenderOptions & RouterOptions
+type CustomRenderOptions = RenderOptions & {
+    store?: TestStore
+} & RouterOptions
 
 export const renderWithProviders = (
     ui: ReactElement,
-    { initialEntries = ['/'], ...options }: CustomRenderOptions = {},
+    {
+        initialEntries = ['/'],
+        store = setupStore(),
+        ...options
+    }: CustomRenderOptions = {},
 ) => {
     return render(
         <Provider store={store}>
